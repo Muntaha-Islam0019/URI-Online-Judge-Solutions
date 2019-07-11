90fdbaddb9ffedb271120d43477d15b85c2b143c
Muntaha Islam <muntaha.islam0019@gmail.com>
2019-07-11 14:32:23 +0600

    Remaining Beginners Problems

---
+++ Beginner/P2635.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n,q;
+    string text;
+    while(cin >> n){
+            vector<string>data;
+        for(int i = 0; i < n ; i++){
+            cin >> text;
+            data.push_back(text);
+        }
+        cin >> q;
+        while(q--){
+            cin >> text;
+            int ans = 0, len = 0;
+            for(int i = 0; i < n ; i++){
+                if(data[i].find(text) == 0){
+                    ans++;
+                    string word = data[i];
+                    len = max(len,(int)word.size());
+                }
+            }
+            if(ans == 0) cout <<  -1 << endl;
+            else cout << ans << " " << len << endl;
+        }
+    }
+    return 0;
+}
