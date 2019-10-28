183cb4224c7e08b57ace806702f3180cd0c75a08
Muntaha Islam <muntaha.islam0019@gmail.com>
2019-10-28 10:22:51 +0600

    Problem 1253

---
+++ Strings/P1253/cmake-build-debug/CMakeFiles/P1253.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/P1253.dir/main.c.obj"
+  "P1253.exe"
+  "P1253.exe.manifest"
+  "P1253.pdb"
+  "libP1253.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/P1253.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
